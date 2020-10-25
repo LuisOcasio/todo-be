21bbd4f069e8c89a852f57815b90a88c90d38082
Luis <ocasio.dev@gmail.com>
2020-10-25 02:40:00 -0400

    seperate api from server

--- api/server.js
+++ api/server.js
@@ -4,8 +4,8 @@ import cors from "cors";
 import helmet from "helmet";
 import morgan from "morgan";
 import passport from "passport";
-require("./config/passport-setup");
-import { Auth, User } from "./routes/index";
+require("../config/passport-setup");
+import { Auth, User } from "../routes/index";
 
 dotenv.config();
 
@@ -15,15 +15,7 @@ server.use(helmet());
 server.use(morgan("combined"));
 server.use(passport.initialize());
 
-const port = process.env.PORT;
-
 server.use("/", User);
 server.use("/auth", Auth);
 
-server.get("/", (req, res) => {
-  res.send(`<h1>This server is up and running.</h1>`);
-});
-
-server.listen(port, () => {
-  console.log(`Server listening at http://localhost:${port}`);
-});
+export default server;
