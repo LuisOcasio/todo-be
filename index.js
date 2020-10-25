54a9292bb310bdbe0ac24e24a62026fa6b6b9295
Luis <ocasio.dev@gmail.com>
2020-10-25 13:51:51 -0400

    initialize sequalize

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 import server from "./api/server";
+import { sequelize } from "./models/index";
 
 const port = process.env.PORT;
 
@@ -6,6 +7,8 @@ server.get("/", (req, res) => {
   res.send(`<h1>This server is up and running.</h1>`);
 });
 
-server.listen(port, () => {
-  console.log(`Server listening at http://localhost:${port}`);
+sequelize.sync().then(() => {
+  server.listen(port, () => {
+    console.log(`Server listening at http://localhost:${port}`);
+  });
 });
