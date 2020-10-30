1483738393c5b8fc57b47ccd749ec3b580f13a01
Luis <ocasio.dev@gmail.com>
2020-10-30 04:32:58 +0000

    create controller for register route. Data is inserted into DB and psw hashed.

--- api/controllers/usersController.js
+++ api/controllers/usersController.js
@@ -1,7 +1,21 @@
 const models = require("../models");
 
-const { user } = models;
+const { User } = models;
 
 //In this file we will define are CRUD functionality for our users
+const registerUser = {
+  async register(req, res, next) {
+    try {
+      const { name, email, password, address, phone } = req.body;
+      const hash = bcrypt.hashSync(password, 10);
+      const user = User.create({ name, address, phone, password: hash });
 
-const users = {};
+      const { id } = user;
+      return res.status(201).send({ user: { id, name, email } });
+    } catch (e) {
+      return next(new Error(e));
+    }
+  },
+};
+
+module.exports = registerUser;
--- routes/user.js
+++ routes/user.js
@@ -1,25 +1,10 @@
 const express = require("express");
-const bcrypt = require("bcrypt");
 const passport = require("passport");
+const { register } = require("../api/controllers/usersController");
 
 const router = express.Router();
 
-router.post("/register", (req, res) => {
-  const credentials = req.body;
-
-  if (credentials.email && credentials.password) {
-    const hash = bcrypt.hashSync(credentials.password, 10);
-    credentials.password = hash;
-  } else {
-    res
-      .status(401)
-      .json({ message: "Please be sure to provide a email and password." });
-  }
-
-  console.log(req.body);
-  res.json(req.body);
-});
-
+router.post("/register", register);
 router.get("/google", passport.authenticate("google", { scope: "profile" }));
 
 module.exports = router;
