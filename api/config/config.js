e60482fd88176ced574020735a61236a78ef0f1b
Luis <ocasio.dev@gmail.com>
2020-10-30 20:36:46 +0000

    config sequelize config

--- api/config/config.js
+++ api/config/config.js
@@ -3,7 +3,7 @@ require("dotenv").config();
 module.exports = {
   development: {
     use_env_variable: "LOCAL_DATABASE",
-    dialect: process.env.DIALECT,
+    dialect: "DIALECT",
   },
   production: {
     use_env_variable: "DATABASE_URL",
