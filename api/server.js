c167d28f2defa0641714f5c21cc5f6d6537984e9
Luis <ocasio.dev@gmail.com>
2020-10-30 18:36:16 +0000

    reconfigure cors implementation

--- api/server.js
+++ api/server.js
@@ -9,21 +9,20 @@ const { Auth, User } = require("../routes/index");
 
 const server = express();
 
+const port = process.env.PORT;
+
 server.use(cors());
 server.use(helmet());
 server.use(morgan("combined"));
 server.use(express.json());
 server.use(passport.initialize());
 
-const port = process.env.PORT;
-
-server.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "*");
-  if (req.method === "OPTIONS") {
-    res.header("Access-Control-Allow-Origin", PUT, POST, PATCH, DELETE, GET);
-  }
-  return res.status(200).json({});
-});
+const corsOptions = {
+  origin: ["Access-Control-Allow-Origin", "*"],
+  methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+  credentials: false,
+  optionsSuccessStatus: 200,
+};
 
 server.use("/", User);
 server.use("/auth", Auth);
