18174a1da1391f05f396ecefea4ae3b2d0b22517
Luis <ocasio.dev@gmail.com>
2020-11-09 04:19:20 +0000

    setup sequelize sync

--- api/server.js
+++ api/server.js
@@ -3,6 +3,7 @@ const express = require("express");
 const cors = require("cors");
 const helmet = require("helmet");
 const morgan = require("morgan");
+const db = require("./models/index")
 const passport = require("passport");
 require("../config/passport-setup");
 const { Auth, User } = require("../routes/index");
@@ -24,8 +25,9 @@ server.get("/", (req, res) => {
   res.send(`<h1>This server is up and running.</h1>`);
 });
 
-server.listen(port, () => {
-  console.log(`Server listening on port ${process.env.PORT}!`);
+db.sequelize.sync({ force: true}).then(async () => {
+server.listen(port, () => 
+  console.log(`Server listening on port ${process.env.PORT}!`))
 });
 
 module.exports = server;
